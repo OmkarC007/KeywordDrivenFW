5598a04865b4a6b15d4375b3fdb99bd4d0233d2b
Om Chikane <chikaneom007@gmail.com>
2022-08-12 17:57:42 +0530

    Added : Automation using Keyword Driven framework

---
+++ src/main/java/com/qa/fb/keyword/base/Base.java
@@ -0,0 +1,46 @@
+package com.qa.fb.keyword.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+
+
+public class Base {
+	
+	public static WebDriver driver;
+	public static Properties prop;
+
+	
+	public WebDriver init_driver(String browserName)  {
+		if(browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver","C:\\Selenium\\geckodriver-64bit\\geckodriver.exe");
+		   if(prop.getProperty("headless").equals("yes")) {
+			   FirefoxOptions options = new FirefoxOptions();
+			   options.addArguments("--headless");
+			   driver= new FirefoxDriver(options);
+		   }else {
+			   driver = new FirefoxDriver();
+		   }
+		}
+		return driver;
+	}
+	
+	public Properties init_properties() {
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream("C:\\Users\\omkar\\eclipse-workspace\\KeywordDrivenHub\\src\\main\\java\\com\\qa\\fb\\keyword\\config\\config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace(); 
+		}
+		return prop;
+	}
+}
